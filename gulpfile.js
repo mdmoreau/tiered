a7717fd25a7310a53fea02d0ad104eedd32ef059
mdmoreau <mdmoreau@gmail.com>
2016-03-17 21:41:52 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var bs = require('browser-sync');
+var gulp = require('gulp');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+gulp.task('html', function() {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'))
+    .on('end', bs.reload);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/sass/tiered.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(bs.stream())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(bs.stream());
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/tiered.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .on('end', bs.reload);
+});
+
+gulp.task('bs', function() {
+  return bs.init({
+    server: 'dist'
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/index.html', gulp.registry().get('html'));
+  gulp.watch('src/sass/tiered.scss', gulp.registry().get('sass'));
+  gulp.watch('src/js/tiered.js',gulp.registry().get('js'));
+});
+
+gulp.task('default', gulp.series(gulp.parallel('html', 'sass', 'js'), gulp.parallel('bs', 'watch')));
